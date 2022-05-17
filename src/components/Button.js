12568b51f638c57a94c9d9f2519547925d20fc81
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-05-17 08:57:43 -0600

    start working on adding buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,10 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+  props.confirm
+    ? (buttonClass += " button--confirm")
+    : (buttonClass = "button");
+
+  return <button className={buttonClass}>{props.children}</button>;
 }

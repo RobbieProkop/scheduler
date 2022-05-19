37fd6d5f9ce2cfe114192f2bc2e9230a4474c6ec
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-05-18 18:48:07 -0600

    start work on the form

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../../styles/Appointment.scss";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+const Form = (props) => {
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
+
+            /*
+          This must be a controlled component
+          your code goes here
+        */
+          />
+        </form>
+        <InterviewerList value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={onCancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Form;

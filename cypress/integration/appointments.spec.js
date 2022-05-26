917f833ee60b884d6e031aa694f2b0a570309ac4
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-05-25 20:12:45 -0600

    add tests for appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,24 @@
-const { CYCLIC_KEY } = require("@storybook/addon-actions");
-
 describe("appointments", () => {
   describe("should book an interview", () => {
-    it("should load the webpage", () => {
+    beforeEach(() => {
       cy.visit("/");
+    });
+    // it("should load the webpage", () => {
+    //   cy.visit("/");
+    // });
+
+    it("Should check to find Monday is selected", () => {
+      cy.contains("[data-testid=day]", "Monday")
+        .click()
+        .should("have.class", "day-list__item--selected");
+    });
+
+    it("should click on the Add button in the second appointment", () => {
+      cy.get("[alt=Add]").first().click();
+    });
+
+    it("should add a name to the input field", () => {
+      cy.contains("[data-]");
     });
   });
 });

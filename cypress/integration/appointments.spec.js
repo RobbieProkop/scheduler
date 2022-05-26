ab79d8d54834a26ef30613bb1ca8e9f33787cad5
Robbie Prokop <iuliiaprokop@Robbies-MacBook-Pro.local>
2022-05-25 22:30:38 -0600

    complete edit test, begin cancel test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,34 +1,45 @@
 describe("appointments", () => {
   describe("should book an interview", () => {
     beforeEach(() => {
+      cy.request("GET", "/api/debug/reset");
       cy.visit("/");
+      cy.contains("[data-testid=day]", "Monday");
     });
 
-    it("Should check to find Monday is selected", () => {
-      cy.contains("[data-testid=day]", "Monday")
-        .click()
-        .should("have.class", "day-list__item--selected");
-    });
-
-    it("should click on the Add button in the second appointment", () => {
+    it("should book an interview", () => {
+      //clicks the add button (img)
       cy.get("[alt=Add]").first().click();
-    });
-
-    it("should add a name to the input field", () => {
-      cy.get("[alt=Add]").first().click();
+      //types RObbie Prokop as student name
       cy.get("[data-testid=student-name-input]").type("Robbie Prokop");
-    });
-
-    it("should click on an interviewer to select it", () => {
-      cy.get("[alt=Add]").first().click();
+      //sets the interviewer to Sylvia Palmer
       cy.get("[alt='Sylvia Palmer']").click();
-    });
-
-    it("should click the save button", () => {
-      cy.get("[alt=Add]").first().click();
-      cy.get("[data-testid=student-name-input]").type("Robbie Prokop");
-      cy.get("[alt='Sylvia Palmer']").click();
+      //clicks the same button
       cy.contains("Save").click();
+      //checks to see if the show page has rendered
+      cy.get(".appointment__card--show").contains("Robbie Prokop");
+      cy.contains(".appointment__card--show", "Sylvia Palmer");
+    });
+
+    it("should edit an interview", () => {
+      //clicks the edit button, disables the waiting for accionability
+      cy.get("[alt=Edit]").first().click({ force: true });
+      //changes interviewer
+      cy.get("[alt='Tori Malcolm']").click();
+      //clears the input, changes the name
+      cy.get("[data-testid=student-name-input]").clear().type("Yulia Prokop");
+      //clicks the same button
+      cy.contains("Save").click();
+      //checks to see if the show page has rendered
+      cy.get(".appointment__card--show").contains("Yulia Prokop");
+      cy.contains(".appointment__card--show", "Tori Malcolm");
+    });
+
+    it.only("should cancel an interview", () => {
+      //clicks the delete button, disables the waiting for accionability
+      cy.get("[alt=Delete]").first().click({ force: true });
+      //clicks the confirm button
+      cy.contains("Confirm").click();
+      cy.contains("not.have.class", "appointment__card--show").not();
     });
   });
 });
